dfed1f79d932805ac24e1775fbdd51cbcbf87972
Yasuhisa Yoshida <syou6162@gmail.com>
2012-03-04 10:59:54 +0900

    clojureのversionを上げて、clj-sys/plumbingのversionを下げた
    clj-sys/plumbingの0.1.5-SNAPSHOTは見つからなかった

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject nlputil-clj "1.1-SNAPSHOT"
-  :dependencies 
-  [[org.clojars.pjt/opennlp-tools "1.4.3"]
-    [org.clojure/clojure "1.2.0"]
-    [org.clojure/clojure-contrib "1.2.0"]
-    [clj-sys/plumbing "0.1.5-SNAPSHOT"]]
-  :aot [mochi.nlp.process.tokenizer]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"})
-   
+  :dependencies [[org.clojars.pjt/opennlp-tools "1.4.3"]
+		 [org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [clj-sys/plumbing "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]])
